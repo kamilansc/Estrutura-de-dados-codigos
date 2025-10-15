6806b13aab79552418675d50dfbebdf9fd7580cb
kamilansc <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>
2025-10-15 00:30:11 -0300

    move funções para o módulo correto

--- Capitulo4 - filas/eh_palindroma.c
+++ Capitulo4 - filas/eh_palindroma.c
@@ -5,58 +5,6 @@
 #include "fila.h"
 #include "../Capitulo2/Pilha.h"
 
-#define avanca(i) (i = (i+1) % F->max)
-
-Fila fila (int m) {
-    Fila F = malloc(sizeof(struct fila));
-    F->max = m;
-    F->total = 0;
-    F->inicio = 0;
-    F->final = 0;
-    F->item = malloc(m*sizeof(Itemf));
-    return F;
-}
-
-
-    int vaziaf(Fila F) {
-        return (F->total == 0);
-    }
-
-
-    int cheiaf (Fila F){
-        return (F->total == F->max);
-    }
-
-
-    void enfileira (Itemf item, Fila F) {
-        if (cheiaf(F)) {
-            puts ("Fila cheia!"); 
-            abort();
-        }
-        F->item[F->final] = item;
-        avanca(F->final);
-        F->total++;
-    }
-
-
-    Itemf desenfileira (Fila F) {
-        if (vaziaf(F)) {
-            puts ("Fila vazia!");
-            abort();
-        }
-        Itemf x = F->item[F->inicio];
-        avanca(F->inicio);
-        F->total--;
-        return x;
-    }
-
-
-    void destroif (Fila *G) {
-        free ((*G)->item);
-        free (*G);
-        *G = NULL;
-    }
-
 
 int main(){
     char s[256];
