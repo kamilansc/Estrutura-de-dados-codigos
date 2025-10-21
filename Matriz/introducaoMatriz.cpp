36831cbd58a6acff4d6ed62d2ba612f8d96953d1
kamilansc <kamilarocha155@gmail.com>
2025-10-21 12:36:45 -0300

    Aprendendo matriz

---
+++ Matriz/introducaoMatriz.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    int matriz[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+    cout << "Matriz criada:" << matriz[1][2] << endl;
+}
